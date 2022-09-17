f405cffb2567397bbcf253dfa39f9fcd032b7c16
mrReilly <thomas@reilly.nl>
2022-09-17 17:03:49 +0200

    first commit

---
+++ src/main/java/com/example/EindOpdrachtBackend/EindOpdrachtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.EindOpdrachtBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindOpdrachtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindOpdrachtBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EindOpdrachtBackend/EindOpdrachtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EindOpdrachtBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindOpdrachtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
